1b4d3b5780ac3d3d0a591b6fbbda8c77eb9bf41d
sana22mav <sana22_mav@student.ztu.edu.ua>
2023-10-23 20:57:02 +0300

    Updated Infrastructure

--- DiscountManagement.Infrastructure/Repositories/OrderRepository.cs
+++ DiscountManagement.Infrastructure/Repositories/OrderRepository.cs
@@ -1,6 +1,48 @@
-namespace DiscountManagement.Infrastructure.Repositories;
+using DiscountManagement.Core.Entities;
+using DiscountManagement.Core.Interfaces;
+using DiscountManagement.Infrastructure.Data;
 
-public class OrderRepository
+namespace DiscountManagement.Infrastructure.Repositories
 {
-    
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext _context;
+
+        public OrderRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Order Get(Guid id)
+        {
+            return _context.Orders.Find(id);
+        }
+
+        public IEnumerable<Order> GetByCustomerId(Guid customerId)
+        {
+            return _context.Orders.Where(o => o.CustomerId == customerId).ToList();
+        }
+
+        public void Add(Order order)
+        {
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+        }
+
+        public void Update(Order order)
+        {
+            _context.Orders.Update(order);
+            _context.SaveChanges();
+        }
+
+        public void Remove(Guid id)
+        {
+            var order = _context.Orders.Find(id);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+                _context.SaveChanges();
+            }
+        }
+    }
 }
--- DiscountManagement.Infrastructure/Repositories/ProductRepository.cs
+++ DiscountManagement.Infrastructure/Repositories/ProductRepository.cs
@@ -1,6 +1,53 @@
-namespace DiscountManagement.Infrastructure.Repositories;
+using DiscountManagement.Core.Entities;
+using DiscountManagement.Core.Interfaces;
+using DiscountManagement.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public class ProductRepository
+namespace DiscountManagement.Infrastructure.Repositories
 {
-    
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Product Get(Guid id)
+        {
+            return _context.Products.Find(id);
+        }
+
+        public IEnumerable<Product> GetByOrderId(Guid orderId)
+        {
+            // Припускаючи, що у вас є відношення між таблицями Products та Orders, 
+            // ви можете використовувати наступний запит:
+            return _context.Products.Where(p => p.OrderId == orderId).ToList();
+        }
+
+        public void Add(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+        }
+
+        public void Update(Product product)
+        {
+            _context.Products.Update(product);
+            _context.SaveChanges();
+        }
+
+        public void Remove(Guid id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+        }
+    }
 }
